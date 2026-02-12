83a4d2056062a9090054736116735ec918763994
nikolay.alipiev@gmail.com <nikolay.alipiev@gmail.com>
2026-02-12 22:26:13 +0200

    feat: add payment service project

---
+++ Lottery.Payment/Program.cs
@@ -0,0 +1,10 @@
+using Lottery.Payment;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
